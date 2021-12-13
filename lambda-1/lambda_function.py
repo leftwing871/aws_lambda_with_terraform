40dd26c958920ebbe2e1eb05ca74eeccbcdf7cf6
Oscar Lee <leftwing871@gmail.com>
2021-12-13 18:23:19 +0900

    x

--- lambda-1/lambda_function.py
+++ lambda-1/lambda_function.py
@@ -5,7 +5,6 @@ def lambda_handler(event, context):
     print("cicd")
     print("cicd")
     print("cicd")
-    print("cicd")
     print(json.dumps(event, indent=2))
     print(json.dumps(event, indent=2))
 
