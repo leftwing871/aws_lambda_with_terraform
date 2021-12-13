8b14eeb1027050daa040060106cc65cc21010688
Oscar Lee <leftwing871@gmail.com>
2021-12-13 17:17:51 +0900

    x

---
+++ lambda-1/lambda_function.py
@@ -0,0 +1,15 @@
+import json
+
+def lambda_handler(event, context):
+    
+    print("cicd")
+    print("cicd")
+    print(json.dumps(event, indent=2))
+    print(json.dumps(event, indent=2))
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
+
