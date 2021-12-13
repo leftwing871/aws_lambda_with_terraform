37eee805c871206bbb2a47366a54afdfe1cb9e5d
Oscar Lee <leftwing871@gmail.com>
2021-12-13 18:40:46 +0900

    x

--- lambda-1/lambda_function.py
+++ lambda-1/lambda_function.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     print("cicd")
     print("cicd")
     print("cicd")
-    
+
     print(json.dumps(event, indent=2))
     print(json.dumps(event, indent=2))
 
@@ -13,6 +13,4 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
-
-
 
